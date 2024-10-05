afe4c04bc736817ac2a33845642a1aec51a7a82f
rf0x3d <yarekfrolov@gmail.com>
2024-10-05 14:29:56 +0400

    fix: count null as non-expired because of headscale postgres usage
    Signed-off-by: rf0x3d <yarekfrolov@gmail.com>

--- app/routes/_data.machines._index/machine.tsx
+++ app/routes/_data.machines._index/machine.tsx
@@ -19,6 +19,7 @@ interface Props {
 export default function MachineRow({ machine, routes, magic, users }: Props) {
 	const expired = machine.expiry === '0001-01-01 00:00:00'
 		|| machine.expiry === '0001-01-01T00:00:00Z'
+		|| machine.expiry === null
 		? false
 		: new Date(machine.expiry).getTime() < Date.now()
 
